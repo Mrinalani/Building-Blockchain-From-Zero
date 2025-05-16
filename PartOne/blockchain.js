68a70fc2130d8edb8953549632c939085f862981
Mrinalani <pandeymrinalani02@gmail.com>
2025-05-16 14:04:15 +0530

    Implement basic Block class with SHA256 hashing and mining

---
+++ PartOne/blockchain.js
@@ -0,0 +1,29 @@
+const crypto = require('crypto'); SHA256 = message => crypto.createHash('sha256').update(message).digest('hex')
+
+class Block {
+    constructor(data = []){
+        this.timestamp = Date.now(),
+        this.data = data,
+        this.hash = this.getHash(),
+        this.prevHash = '',
+        this.nonce = 0
+    };
+
+    getHash() {
+        return SHA256(this.timestamp + JSON.stringify(this.data) + this.prevHash + this.nonce);
+    }
+
+    mine(difficulty) {
+        while(!this.hash.startsWith(Array(difficulty + 1).join('0'))) {
+            this.nonce++;
+
+            this.hash = this.getHash();
+        }
+    }
+
+
+}
+
+const block1 = new Block(["Transaction 1"]);
+block1.mine(5);
+console.log(block1);
